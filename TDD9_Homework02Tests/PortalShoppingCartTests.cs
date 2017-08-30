cf915239b84015bb7ee103c5d64ad40803f8e43b
lijenyan <lijenyan@yahoo.com.tw>
2017-08-30 10:08:18 +0800

    加入兩個測試案例 Scenario: 選便宜的算法, 兩套 5 + 4  Given 第一集買了 2 本  And 第二集買了 2 本  And 第三集買了 2 本  And 第四集買了 2 本  And 第五集買了 1 本  When 結帳  Then 價格應為 5本+4本 = 695 元
    Scenario: 選便宜的算法，四套 5+3+5+3
     Given 第一集買了 4 本
     And 第二集買了 4 本
     And 第三集買了 4 本
     And 第四集買了 2 本
     And 第五集買了 2 本
     When 結帳
     Then 價格應為 640*2=1280 元
    
    綠燈 通過

--- TDD9_Homework02Tests/PortalShoppingCartTests.cs
+++ TDD9_Homework02Tests/PortalShoppingCartTests.cs
@@ -57,7 +57,7 @@ namespace TDD9_Homework02.Tests
         }
 
         [TestMethod()]
-        public void GetTotalPriceTest_Buy_3_Diff_Book_Discount_10Percentage_TotalPrice_270_()
+        public void GetTotalPriceTest_Buy_3_Diff_Book_Discount_10Percentage_TotalPrice_270()
         {
             //arrange
             List<Book> Books = new List<Book>
@@ -79,7 +79,7 @@ namespace TDD9_Homework02.Tests
         }
 
         [TestMethod()]
-        public void GetTotalPriceTest_Buy_4_Diff_Book_Discount_20Percentage_TotalPrice_320_()
+        public void GetTotalPriceTest_Buy_4_Diff_Book_Discount_20Percentage_TotalPrice_320()
         {
             //arrange
             List<Book> Books = new List<Book>
@@ -101,7 +101,7 @@ namespace TDD9_Homework02.Tests
         }
 
         [TestMethod()]
-        public void GetTotalPriceTest_Buy_5_Diff_Book_Discount_25Percentage_TotalPrice_375_()
+        public void GetTotalPriceTest_Buy_5_Diff_Book_Discount_25Percentage_TotalPrice_375()
         {
             //arrange
             List<Book> Books = new List<Book>
@@ -123,7 +123,7 @@ namespace TDD9_Homework02.Tests
         }
 
         [TestMethod()]
-        public void GetTotalPriceTest_Buy_1st_1Qty_2nd_1Qty_3rd_2Qty_TotalPrice_370_()
+        public void GetTotalPriceTest_Buy_1st_1Qty_2nd_1Qty_3rd_2Qty_TotalPrice_370()
         {
             //arrange
             List<Book> Books = new List<Book>
@@ -145,7 +145,7 @@ namespace TDD9_Homework02.Tests
         }
 
         [TestMethod()]
-        public void GetTotalPriceTest_Buy_1st_1Qty_2nd_2Qty_3rd_2Qty_TotalPrice_460_()
+        public void GetTotalPriceTest_Buy_1st_1Qty_2nd_2Qty_3rd_2Qty_TotalPrice_460()
         {
             //arrange
             List<Book> Books = new List<Book>
@@ -167,7 +167,7 @@ namespace TDD9_Homework02.Tests
         }
 
         [TestMethod()]
-        public void GetTotalPriceTest_Buy_1st_2Qty_2nd_2Qty_3rd_2Qty_4th_1Qty_5th_1Qty_TotalPrice_640_()
+        public void GetTotalPriceTest_Buy_1st_2Qty_2nd_2Qty_3rd_2Qty_4th_1Qty_5th_1Qty_TotalPrice_640()
         {
             //arrange
             List<Book> Books = new List<Book>
@@ -180,6 +180,50 @@ namespace TDD9_Homework02.Tests
             };
             var target = new PortalShoppingCart(unitOfPrice);
             decimal expected = 640.0M;
+
+            //act
+            decimal actual = target.FindTheLowerTotalPrice(Books);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetTotalPriceTest_Buy_1st_2Qty_2nd_2Qty_3rd_2Qty_4th_2Qty_5th_1Qty_TotalPrice_695()
+        {
+            //arrange
+            List<Book> Books = new List<Book>
+            {
+                new Book { Name = HarryPortalBooksName.Series1st,Unit=2},
+                new Book { Name = HarryPortalBooksName.Series2nd,Unit=2},
+                new Book { Name = HarryPortalBooksName.Series3rd,Unit=2},
+                new Book { Name = HarryPortalBooksName.Series4th,Unit=2},
+                new Book { Name = HarryPortalBooksName.Series5th,Unit=1}
+            };
+            var target = new PortalShoppingCart(unitOfPrice);
+            decimal expected = 695.0M;
+
+            //act
+            decimal actual = target.FindTheLowerTotalPrice(Books);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetTotalPriceTest_Buy_1st_4Qty_2nd_4Qty_3rd_4Qty_4th_2Qty_5th_2Qty_TotalPrice_1280()
+        {
+            //arrange
+            List<Book> Books = new List<Book>
+            {
+                new Book { Name = HarryPortalBooksName.Series1st,Unit=4},
+                new Book { Name = HarryPortalBooksName.Series2nd,Unit=4},
+                new Book { Name = HarryPortalBooksName.Series3rd,Unit=4},
+                new Book { Name = HarryPortalBooksName.Series4th,Unit=2},
+                new Book { Name = HarryPortalBooksName.Series5th,Unit=2}
+            };
+            var target = new PortalShoppingCart(unitOfPrice);
+            decimal expected = 1280.0M;
 
             //act
             decimal actual = target.FindTheLowerTotalPrice(Books);
