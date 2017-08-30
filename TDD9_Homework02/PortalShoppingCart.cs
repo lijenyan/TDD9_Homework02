82ea83587a0cb7c5855db96d8124425b78506cbf
lijenyan <lijenyan@yahoo.com.tw>
2017-08-30 09:52:08 +0800

    大重構 測試綠燈 通過

--- TDD9_Homework02/PortalShoppingCart.cs
+++ TDD9_Homework02/PortalShoppingCart.cs
@@ -14,117 +14,22 @@ namespace TDD9_Homework02
             _UnitOfPrice = UnitOfPrice;
         }
 
-        private decimal CalPrice(int BooksQty)
-        {
-            decimal discount = GetDiscount(BooksQty);
-            return (1 - discount) * BooksQty * _UnitOfPrice;
-        }
-
         public decimal FindTheLowerTotalPrice(List<Book> originalBooks)
         {
             List<decimal> allPossiblePrice = new List<decimal>();
-            List<Book> moreThanOneUnitBooks = null;
-            List<Book> onlyOneUnitBooks = null;
-            List<Book> calBooks = new List<Book>(originalBooks.Select(x => x.Clone()));
+            //Normal Case
+            CalculatePrice calculatePriceNormalCase = new CalculatePrice(originalBooks, _UnitOfPrice, false);
+            decimal price = calculatePriceNormalCase.TryPossibleCalculateCase();
+            allPossiblePrice.Add(price);
 
-            int originalBooksQty = originalBooks.Where(x => x.Unit > 0).Select(x => x.Unit).Sum();
-            decimal forCaculateEachPrice = 0;
-            int forDiscountQty = 0;
-            int forCaculateTotalQty = originalBooksQty;
-
-            while (forCaculateTotalQty > 0)
-            {
-                //兩本(以上)相同的書是沒有折扣,故先將其拿出來和其他一本的書計算總價
-                moreThanOneUnitBooks = calBooks.Where(x => x.Unit > 1).ToList<Book>();
-                onlyOneUnitBooks = calBooks.Where(x => x.Unit == 1).ToList<Book>();
-
-                foreach (var item in moreThanOneUnitBooks)
-                {
-                    forDiscountQty++;
-                    forCaculateTotalQty--;
-                    foreach (Book book in calBooks)
-                    {
-                        if (book.Name == item.Name && book.Unit > 0)
-                            book.Unit = book.Unit - 1;
-                    }
-                }
-
-                foreach (var item in onlyOneUnitBooks)
-                {
-                    forDiscountQty++;
-                    forCaculateTotalQty--;
-                    foreach (Book book in calBooks)
-                    {
-                        if (book.Name == item.Name && book.Unit > 0)
-                            book.Unit = book.Unit - 1;
-                    }
-                }
-
-                forCaculateEachPrice += CalPrice(forDiscountQty);
-                forDiscountQty = 0;
-            }
-            allPossiblePrice.Add(forCaculateEachPrice);
-            forCaculateEachPrice = 0;
-
-            ////特例:計算4本或5本為一組是否有更低價
-            calBooks = new List<Book>(originalBooks.Select(x => x.Clone()));
-            forCaculateTotalQty = originalBooksQty;
-            while (forCaculateTotalQty > 0)
-            {
-                //兩本(以上)相同的書是沒有折扣,故先將其拿出來和其他一本的書計算總價
-                moreThanOneUnitBooks = calBooks.Where(x => x.Unit > 1).ToList<Book>();
-                onlyOneUnitBooks = calBooks.Where(x => x.Unit == 1).ToList<Book>();
-
-                foreach (var item in moreThanOneUnitBooks)
-                {
-                    if (forDiscountQty == 4)
-                        break;
-                    forDiscountQty++;
-                    forCaculateTotalQty--;
-                    foreach (Book book in calBooks)
-                    {
-                        if (book.Name == item.Name && book.Unit > 0)
-                            book.Unit = book.Unit - 1;
-                    }
-                }
-
-                foreach (var item in onlyOneUnitBooks)
-                {
-                    if (forDiscountQty == 4)
-                        break;
-                    forDiscountQty++;
-                    forCaculateTotalQty--;
-                    foreach (Book book in calBooks)
-                    {
-                        if (book.Name == item.Name && book.Unit > 0)
-                            book.Unit = book.Unit - 1;
-                    }
-                }
-
-                forCaculateEachPrice += CalPrice(forDiscountQty);
-                forDiscountQty = 0;
-            }
-            allPossiblePrice.Add(forCaculateEachPrice);
+            //Special Case
+            CalculatePrice calculatePriceSpecialCase = new CalculatePrice(originalBooks, _UnitOfPrice, true);
+            price = calculatePriceSpecialCase.TryPossibleCalculateCase();
+            allPossiblePrice.Add(price);
 
             decimal theLowestPrice = allPossiblePrice.OrderBy(x => x).FirstOrDefault();
             allPossiblePrice.Clear();
             return theLowestPrice;
-        }
-
-        
-
-        private decimal GetDiscount(int qty)
-        {
-            decimal[] discount = new decimal[] { 0, 0.05M, 0.1M, 0.2M, 0.25M };
-            return discount[qty - 1];
-        }
-    }
-
-    static class Extensions
-    {
-        public static IList<T> Clone<T>(this IList<T> listToClone) where T : ICloneable
-        {
-            return listToClone.Select(item => (T)item.Clone()).ToList();
         }
     }
 
@@ -146,5 +51,74 @@ namespace TDD9_Homework02
     public enum HarryPortalBooksName
     {
         Series1st, Series2nd, Series3rd, Series4th, Series5th
+    }
+
+    public class CalculatePrice
+    {
+        public CalculatePrice(List<Book> OriginalBooks, decimal UnitOfPrice,bool IsSpecialCase)
+        {
+            this.CloneOriginalBooks = new List<Book>(OriginalBooks.Select(x => x.Clone()));
+            this.UnitOfPrice = UnitOfPrice;
+            this.TotalBookQty = OriginalBooks.Where(x => x.Unit > 0).Select(x => x.Unit).Sum();
+            this.EachCaculatedPrice = 0;
+            this.IsSpecialCase = IsSpecialCase;
+        }
+        public bool IsSpecialCase { get; set; }
+        private decimal UnitOfPrice { get; set; }
+        private int TotalBookQty { get; set; }
+        private int DiscountBookQty { get; set; }
+
+        private decimal EachCaculatedPrice { get; set; }
+
+        private List<Book> MoreThanOneUnitBooks { get; set; }
+        private List<Book> OnlyOneUnitBooks { get; set; }
+
+        private List<Book> CloneOriginalBooks { get; set; }
+
+        public decimal TryPossibleCalculateCase()
+        {
+            while (TotalBookQty > 0)
+            {
+                //兩本(以上)相同的書是沒有折扣,故先將其拿出來和其他一本的書計算總價
+                MoreThanOneUnitBooks = CloneOriginalBooks.Where(x => x.Unit > 1).ToList<Book>();
+                OnlyOneUnitBooks = CloneOriginalBooks.Where(x => x.Unit == 1).ToList<Book>();
+
+                TakeBooksForMatchDiscountBookQty(MoreThanOneUnitBooks);
+                TakeBooksForMatchDiscountBookQty(OnlyOneUnitBooks);
+
+                EachCaculatedPrice += CalculateDiscountTimeQtyTimeUnitOfPirce(DiscountBookQty);
+                DiscountBookQty = 0;
+            }
+           
+            return EachCaculatedPrice;
+        }
+
+        private void TakeBooksForMatchDiscountBookQty(List<Book> Books)
+        {
+            foreach (var item in Books)
+            {
+                if (IsSpecialCase && DiscountBookQty == 4)
+                    break;
+                DiscountBookQty++;
+                TotalBookQty--;
+                foreach (Book book in CloneOriginalBooks)
+                {
+                    if (book.Name == item.Name && book.Unit > 0)
+                        book.Unit = book.Unit - 1;
+                }
+            }
+        }
+
+        private decimal CalculateDiscountTimeQtyTimeUnitOfPirce(int BooksQty)
+        {
+            decimal discount = GetDiscount(BooksQty);
+            return (1 - discount) * BooksQty * UnitOfPrice;
+        }
+
+        private decimal GetDiscount(int qty)
+        {
+            decimal[] discount = new decimal[] { 0, 0.05M, 0.1M, 0.2M, 0.25M };
+            return discount[qty - 1];
+        }
     }
 }
